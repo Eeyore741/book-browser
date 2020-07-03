5fc3e3f01d404452f20392e2dc339e64696c09b0
Vitalii Kuznetsov <v-blacksmith@hotmail.com>
2020-07-03 17:04:18 +0200

    Create Xcode project

---
+++ ios-project/book-browser/book-browser/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  book-browser
+//
+//  Created by Vitalii Kuznetsov on 03/07/2020.
+//  Copyright © 2020 Vitalii Kuznetsov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
