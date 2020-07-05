1bdc85060a79fb8b959693fe18493fd0ecb9831a
Vitalii Kuznetsov <v-blacksmith@hotmail.com>
2020-07-05 20:27:43 +0200

    Refactor InputFlowCoordinator with InputView & model

--- ios-project/book-browser/book-browser/Sources/Features/Input/InputFlowCoordinator.swift
+++ ios-project/book-browser/book-browser/Sources/Features/Input/InputFlowCoordinator.swift
@@ -6,10 +6,38 @@
 //  Copyright © 2020 Vitalii Kuznetsov. All rights reserved.
 //
 
-import Foundation
+import UIKit
 
 /// Flow coordinator to handle user text input 
 final class InputFlowCoordinator: FlowCoordinator {
     
     var childFlowCoordinators: [FlowCoordinator] = []
+    
+    weak var parentController: UINavigationController?
+    private var viewController: UIViewController?
+    
+    var onInputDone: ((String?) -> ())?
+    
+    init(parentController: UINavigationController) {
+        self.parentController = parentController
+    }
+    
+    public func start() {
+        let model = InputViewModel()
+        model.backgroundColor = { UIColor.yellow }
+        model.textBackgroundColor = { UIColor.lightText }
+        model.textColor = { UIColor.darkGray }
+        model.onTextViewDone = { (text: String?) in
+            self.onInputDone?(text)
+        }
+        let view = InputView(viewModel: model)
+        let controller = BasicViewController(view: view)
+        controller.modalPresentationStyle = .fullScreen
+        self.parentController?.present(controller, animated: false)
+        self.viewController = controller
+    }
+    
+    public func finish(completionHandler: (() -> Void)? = nil) {
+        self.viewController?.dismiss(animated: false, completion: completionHandler)
+    }
 }
