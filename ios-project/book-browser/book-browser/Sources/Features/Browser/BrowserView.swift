1bf704c572b8b938abee3e6a421bfc0762753427
Vitalii Kuznetsov <v-blacksmith@hotmail.com>
2020-07-10 22:43:25 +0200

    Define BrowserView

---
+++ ios-project/book-browser/book-browser/Sources/Features/Browser/BrowserView.swift
@@ -0,0 +1,64 @@
+//
+//  BrowserView.swift
+//  book-browser
+//
+//  Created by Vitalii Kuznetsov on 10/07/2020.
+//  Copyright © 2020 Vitalii Kuznetsov. All rights reserved.
+//
+
+import UIKit
+
+final class BrowserView: UITableView {
+    
+    override class var requiresConstraintBasedLayout: Bool { true }
+    
+    public var viewModel: BrowserViewModel
+    
+    init(viewModel: BrowserViewModel) {
+        self.viewModel = viewModel
+        super.init(frame: CGRect.zero, style: UITableView.Style.plain)
+        self.backgroundColor = self.viewModel.backgroundColor
+        self.tintColor = self.viewModel.backgroundColor
+        self.backgroundView?.backgroundColor = self.viewModel.backgroundColor
+        self.register(self.viewModel.cellType, forCellReuseIdentifier: self.viewModel.cellType.reuseIdentifier)
+        self.rowHeight = self.viewModel.cellHeight
+    }
+    
+    @available(*, unavailable)
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
+/// `BrowserView` is data source for itself to clear our model from redundant requirements
+/// like being a `reference type` and conform to `NSObjectProtocol`
+extension BrowserView: UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        guard tableView == self else { fatalError("Instance should only be a data source for itself") }
+        
+        return Int.random(in: 1...10)
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard tableView == self else { fatalError("Instance should only be a data source for itself") }
+        
+        return tableView.dequeueReusableCell(withIdentifier: self.viewModel.cellType.reuseIdentifier, for: indexPath)
+    }
+}
+
+/// `BrowserView` is delegate for itself to clear our model from redundant requirements
+/// like being a `reference type` and conform to `NSObjectProtocol`
+extension BrowserView: UITableViewDelegate {
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        self.viewModel.onSelect() // TODO: fill with model
+    }
+}
+
+/// Private helper extension with file-only usefull functions
+private extension UITableViewCell {
+    class var reuseIdentifier: String {
+        String(describing: self)
+    }
+}
