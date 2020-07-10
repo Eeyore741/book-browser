897641207c3b883f30d1bdee4776dcadfa6a649f
Vitalii Kuznetsov <v-blacksmith@hotmail.com>
2020-07-10 22:42:29 +0200

    Extend BrowserFlowCoordinator with initial display behaviour

--- ios-project/book-browser/book-browser/Sources/Features/Browser/BrowserFlowCoordinator.swift
+++ ios-project/book-browser/book-browser/Sources/Features/Browser/BrowserFlowCoordinator.swift
@@ -6,10 +6,33 @@
 //  Copyright © 2020 Vitalii Kuznetsov. All rights reserved.
 //
 
-import Foundation
+import UIKit
 
 /// Flow coordinator handling books list displaying and user list interactions
 final class BrowserFlowCoordinator: FlowCoordinator {
     
     var childFlowCoordinators: [FlowCoordinator] = []
+    
+    weak var parentController: UINavigationController?
+    private var viewController: UIViewController?
+    
+    var onClose: ((String?) -> ())?
+    var onSelection: ((String?) -> ())?
+    
+    init(parentController: UINavigationController) {
+        self.parentController = parentController
+    }
+    
+    public func start() {
+        var model = BrowserViewModel()
+        model.backgroundColor = UIColor.red
+        let view = BrowserView(viewModel: model)
+        let controller = BasicViewController(view: view)
+        controller.modalPresentationStyle = UIModalPresentationStyle.fullScreen
+        self.parentController?.present(controller, animated: false)
+    }
+    
+    public func finish() {
+        
+    }
 }
