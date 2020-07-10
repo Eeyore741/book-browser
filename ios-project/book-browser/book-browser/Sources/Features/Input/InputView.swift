7b83f45fc38aff495d339468ad67782436487e9f
Vitalii Kuznetsov <v-blacksmith@hotmail.com>
2020-07-10 22:41:24 +0200

    Deprecate InputView required init

--- ios-project/book-browser/book-browser/Sources/Features/Input/InputView.swift
+++ ios-project/book-browser/book-browser/Sources/Features/Input/InputView.swift
@@ -24,6 +24,7 @@ final class InputView: UIView {
         super.init(frame: CGRect.zero)
     }
     
+    @available(*, unavailable)
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
