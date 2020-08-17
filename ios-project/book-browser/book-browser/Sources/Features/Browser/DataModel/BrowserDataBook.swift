2a8357259b88df6a85c4fba1e0e72e695bddc5ff
Vitalii Kuznetsov <v-blacksmith@hotmail.com>
2020-08-17 21:24:38 +0200

    Define basic book model

---
+++ ios-project/book-browser/book-browser/Sources/Features/Browser/DataModel/BrowserDataBook.swift
@@ -0,0 +1,17 @@
+//
+//  BrowserDataBook.swift
+//  book-browser
+//
+//  Created by Vitalii Kuznetsov on 17/08/2020.
+//  Copyright © 2020 Vitalii Kuznetsov. All rights reserved.
+//
+
+import Foundation
+
+/// Basic book model
+struct BrowserDataBook {
+    let id: String
+    let author: String
+    let title: String
+    let imageUrl: String
+}
