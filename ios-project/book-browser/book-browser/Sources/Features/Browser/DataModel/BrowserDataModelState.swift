25327beeeae949c8ef8990586cac1167ad767f06
Vitalii Kuznetsov <v-blacksmith@hotmail.com>
2020-08-20 20:24:23 +0200

    Define state type for book data model

---
+++ ios-project/book-browser/book-browser/Sources/Features/Browser/DataModel/BrowserDataModelState.swift
@@ -0,0 +1,17 @@
+//
+//  BrowserDataModelState.swift
+//  book-browser
+//
+//  Created by Vitalii Kuznetsov on 20/08/2020.
+//  Copyright © 2020 Vitalii Kuznetsov. All rights reserved.
+//
+
+import Foundation
+
+/// Type representing state of data model instance lifecycle
+enum BrowserDataModelState {
+    
+    case inactive // Ready for calls
+    case active // Instance performing task
+    case error(error: BrowserDataError) // Inactive with error
+}
