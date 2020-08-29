813be8bc145879817a47e7a541de5aec6e9624da
Vitalii Kuznetsov <v-blacksmith@hotmail.com>
2020-08-29 22:05:57 +0200

    Define browser data response value type

---
+++ ios-project/book-browser/book-browser/Sources/Features/Browser/DataModel/BrowserDataResponse.swift
@@ -0,0 +1,19 @@
+//
+//  BrowserDataResponse.swift
+//  book-browser
+//
+//  Created by Vitalii Kuznetsov on 29/08/2020.
+//  Copyright © 2020 Vitalii Kuznetsov. All rights reserved.
+//
+
+import Foundation
+
+/// Type wrapping response content in case of success
+struct BrowserDataResponse {
+    
+    let books: [BrowserDataBook]
+    let query: String
+}
+
+/// Conform to `Equatable` so encapsulating types can also conform
+extension BrowserDataResponse: Equatable { }
