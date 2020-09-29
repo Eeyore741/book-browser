e9350cdaae21897f90a657bde5e426bfe7bf195a
Vitalii Kuznetsov <v-blacksmith@hotmail.com>
2020-09-29 22:09:06 +0200

    Define book list response

---
+++ ios-project/book-browser/book-browser/Sources/Features/Browser/DataModel/Remote/API/BookListResponse.swift
@@ -0,0 +1,23 @@
+//
+//  BookListResponse.swift
+//  book-browser
+//
+//  Created by Vitalii Kuznetsov on 29/09/2020.
+//  Copyright © 2020 Vitalii Kuznetsov. All rights reserved.
+//
+
+import Foundation
+
+/// Response object expected to be used for decoding data in response for `BookListRequest`
+struct BookListResponse {
+    
+    let query: String
+    let filter: String
+    let count: Int
+    let totalCount: Int
+    let items: [BrowserDataBook]
+}
+
+/// Conforming book model to `Codable` getting functionality of
+/// decoding model from raw data
+extension BookListResponse: Decodable { }
