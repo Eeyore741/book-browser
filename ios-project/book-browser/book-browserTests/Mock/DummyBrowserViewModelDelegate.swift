527c369d78915daed7343b6346efb71afc4b8f62
Vitalii Kuznetsov <v-blacksmith@hotmail.com>
2020-12-06 10:27:30 +0100

    Define dumme browser data model delegate

---
+++ ios-project/book-browser/book-browserTests/Mock/DummyBrowserViewModelDelegate.swift
@@ -0,0 +1,22 @@
+//
+//  DummyBrowserViewModelDelegate.swift
+//  book-browserTests
+//
+//  Created by Vitalii Kuznetsov on 03/12/2020.
+//  Copyright © 2020 Vitalii Kuznetsov. All rights reserved.
+//
+
+import Foundation
+@testable import book_browser
+
+final class DummyBrowserViewModelDelegate: BrowserViewModelDelegate {
+    
+    var onViewModelStateChanged: (() -> Void)?
+    var delay: TimeInterval = 0
+    
+    func viewModelStateChanged(_ model: BrowserViewModel) {
+        DispatchQueue.main.asyncAfter(deadline: DispatchTime.now()+self.delay) {
+            self.onViewModelStateChanged?()
+        }
+    }
+}
