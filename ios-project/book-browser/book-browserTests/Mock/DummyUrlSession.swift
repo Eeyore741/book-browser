d09f7b7451877ec40d5605c20ac3531f4510ffe0
Vitalii Kuznetsov <v-blacksmith@hotmail.com>
2020-12-12 21:46:53 +0100

    Define dummy url session

---
+++ ios-project/book-browser/book-browserTests/Mock/DummyUrlSession.swift
@@ -0,0 +1,39 @@
+//
+//  DummyUrlSession.swift
+//  book-browserTests
+//
+//  Created by Vitalii Kuznetsov on 11/12/2020.
+//  Copyright © 2020 Vitalii Kuznetsov. All rights reserved.
+//
+
+import Foundation
+
+final class DummyUrlSession: URLSession {
+    
+    override func dataTask(with request: URLRequest, completionHandler: @escaping (Data?, URLResponse?, Error?) -> Void) -> URLSessionDataTask {
+        
+        let task = DummyURLSessionDataTask()
+        task.delay = self.delay
+        
+        switch self.behaviour {
+        case let .fine(data):
+            task.onResume = { completionHandler(data, nil, nil) }
+        case let .fail(error):
+            task.onResume = { completionHandler(nil, nil, error) }
+        }
+
+        return task
+    }
+    
+    /// Dummy helpers
+    enum Behaviour {
+        case fine(Data)
+        case fail(Error)
+    }
+    
+    var behaviour: Behaviour = .fine(Data())
+    
+    var delay: TimeInterval = 1
+    
+    var onDataTask: ((URLRequest, (Data?, URLResponse?, Error?) -> Void) -> URLSessionDataTask) = { _,_ in fatalError() }
+}
